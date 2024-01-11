b4ff77dc9ce92578101cefb1575eb01cef694b7c
alexgg9 <alexgalvezgarciaqt@gmail.com>
2024-01-11 16:07:13 +0100

    RaceClassFinish

--- src/main/java/com/example/motogp/Race.java
+++ src/main/java/com/example/motogp/Race.java
@@ -1,18 +1,23 @@
 package com.example.motogp;
 
 import com.example.motogp.model.Bike;
-
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Race {
-    private static final int NUM_MOTOCICLISTAS = 10;
+    public static final int NUM_MOTOCICLISTAS = 10;
     public static final int DISTANCIA_CARRERA = 100;
-
-    public static int posicion[] = new int[NUM_MOTOCICLISTAS];
+    public static AtomicInteger[] posicion;
     public static Random random = new Random();
 
-    public static void main(String[] args) throws InterruptedException {
+
+    private static void iniciarCarrera() throws InterruptedException {
         Thread[] motociclistas = new Thread[NUM_MOTOCICLISTAS];
+
+        posicion = new AtomicInteger[NUM_MOTOCICLISTAS];
+        for (int i = 0; i < NUM_MOTOCICLISTAS; i++) {
+            posicion[i] = new AtomicInteger(0);
+        }
 
         for (int i = 0; i < NUM_MOTOCICLISTAS; i++) {
             motociclistas[i] = new Thread(new Bike(i));
@@ -21,11 +26,6 @@ public class Race {
 
         for (int i = 0; i < NUM_MOTOCICLISTAS; i++) {
             motociclistas[i].join();
-        }
-
-        System.out.println("Resultados finales:");
-        for (int i = 0; i < NUM_MOTOCICLISTAS; i++) {
-            System.out.println("Motociclista " + i + ": " + posicion[i]);
         }
     }
 }
